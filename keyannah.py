49c56da786b38c35ec6222d0a43d67719a057652
Kiante Moore <myboyknuckles@gmail.com>
2020-11-09 20:02:22 -0600

    Create keyannah.py

---
+++ keyannah.py
@@ -0,0 +1,10 @@
+keyannah = input("What is Keyannah doing?  ")
+currency = 0
+
+if keyannah == "laying" and "studying":
+    print("This is facts. Keyannah is indeed doing what she is suppose to be doing.")
+    currency += 1
+    print("She deserves a reward. We will give her 1 dollar.")
+    print("current amount she has earned is {}".format(currency))
+else if keyannah != "laying" or "studying":
+    print("Keyannah is procrastinating once again. Go get the belt.")
